763529cdbb03b6dfee382c8e2d7d37221845c815
buhari44 <adeyemibuari@gmail.com>
2021-05-02 05:40:57 +0100

    making the accordion dynamic

--- js/questions.js
+++ js/questions.js
@@ -1,13 +1,33 @@
 // FILE CONTAINING OBJECTS OF QUESTIONS WITH THEIR ANSWERS
-const questions = [
-    {
+const questions = [{
         id: 1,
-        question: "What is a man doing here ?",
-        answer: "I am coming home soon"
+        question: "What is a man doing here What is a man doing here What is a man doing here What is a man doing here What is a man doing here ? ",
+        answer: "I am coming home soon",
     },
     {
         id: 2,
         question: "This is a dummy sample?",
-        answer: "you can replace me"
+        answer: "you can replace me",
     },
-]
+];
+
+const accordion = document.getElementById("accord");
+let createEle = "";
+questions.map((item, index) => {
+    const { id, question, answer } = item;
+    createEle += `<div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+       ${id}.   ${question}
+      </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        ${answer}
+                    </div>
+                </div>
+            </div>
+        </div>`;
+});
+accordion.innerHTML = createEle;
